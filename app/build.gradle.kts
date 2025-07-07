ae2b4b6ee5a9338cdda65651d3079a41514a2226
Art laptop <lyndonehemplo1@gmail.com>
2025-07-07 16:19:55 +0800

    added zxing embedded version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation("com.github.skydoves:colorpicker-compose:1.1.2")
 
     implementation ("com.google.zxing:core:3.5.1")
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
 }
